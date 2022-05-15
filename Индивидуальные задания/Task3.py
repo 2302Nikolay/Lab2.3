379ba5d391b2d0b0c822ac6c534cf3951bc523fa
2302Nikolay <Malzewn@gmail.com>
2022-05-15 16:52:49 +0300

    Доделал индивид задание 3

--- Индивидуальные задания/Task3.py
+++ Индивидуальные задания/Task3.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+word = input("Введите слово: ")
+char = input("Введите букву: ")
+num = int(input("Введите номер: "))
+
+s = (word[0:num]+char+word[num:])
+print(s)
