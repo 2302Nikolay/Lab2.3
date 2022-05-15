303da553f09426e6d20079d45e41477fdbc47b64
2302Nikolay <Malzewn@gmail.com>
2022-05-15 18:48:15 +0300

    Выполнил задания повышенной сложности

---
+++ Задание повышенной сложности/HardTask1_V2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+word_1 = input("Введите первое слово: ")
+word_2 = input("Введите второе слово: ")
+
+word_1 = word_1.lower()
+word_2 = word_2.lower()
+
+list_word1 = list(word_1)
+list_word2 = list(word_2)
+
+list_word1.sort()
+list_word2.sort()
+
+if list_word1 == list_word2:
+    print("Можно")
+else:
+    print("Нельзя")
